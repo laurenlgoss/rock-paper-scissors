785f2ecb0f243a509d3477698c47ba4deb3322e5
Lauren Goss <laurenlgoss98@gmail.com>
2022-11-24 12:07:42 -0500

    started win/lose logic

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import inquirer from 'inquirer';
 
+const choices = ['Rock', 'Paper', 'Scissors'];
+
 function init() {
   gameStart();
 }
@@ -10,13 +12,62 @@ function gameStart() {
       {
         type: 'list',
         message: 'Rock, paper, or scissors?',
-        choices: ['Rock', 'Paper', 'Scissors'],
+        choices: choices,
         name: 'choice',
       },
     ])
     .then((response) => {
-      console.log(response.choice);
+      const computerChoice = getComputerChoice(choices);
+      console.log(`Computer chose ${computerChoice}`);
+
+      calcWinOrLose(response.choice, computerChoice);
+    })
+    .catch((error) => {
+      console.log(error);
     });
+}
+
+function getComputerChoice(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+function calcWinOrLose(userChoice, computerChoice) {
+  if (userChoice === computerChoice) {
+    console.log('Tie!');
+    return;
+  }
+
+  switch (userChoice) {
+    case 'Rock':
+      if (computerChoice === 'Paper') {
+        userLoses();
+      } else if (computerChoice === 'Scissors') {
+        userWins();
+      }
+      break;
+    case 'Paper':
+      if (computerChoice === 'Rock') {
+        userWins();
+      } else if (computerChoice === 'Scissors') {
+        userLoses();
+      }
+      break;
+    case 'Scissors':
+      if (computerChoice === 'Rock') {
+        userLoses();
+      } else if (computerChoice === 'Paper') {
+        userWins();
+      }
+      break;
+  }
+}
+
+function userWins() {
+  console.log('You won!');
+}
+
+function userLoses() {
+  console.log('You lost!');
 }
 
 init();
